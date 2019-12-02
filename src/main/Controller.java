caa8fb4d24f10eb9a58cc1f75384e981887addea
horseburger <pralka.gniewu@gmail.com>
2019-12-02 12:03:09 +0100

    feat: add movie objects to Controller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,19 +1,25 @@
 package main;
 
-import com.sun.javafx.collections.ObservableListWrapper;
-import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
-
-import java.util.ArrayList;
-import java.util.Observable;
+import javafx.scene.control.TabPane;
 
 public class Controller {
 
     @FXML
-    private ListView<Button> moviesList;
+    private TabPane panes;
+
+    @FXML
+    private ListView<?> moviesPane;
+
+    @FXML
+    private Button addMovie;
+
+    @FXML
+    private ListView<?> roomPane;
+
+    @FXML
+    private ListView<?> showPane;
 
 }
