087b16dc41a54e1c5082098aa75e9b66aaafc9fb
horseburger <pralka.gniewu@gmail.com>
2020-01-04 17:27:13 +0100

    feat: some retrofit shenanigans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -17,6 +24,22 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+
+//        launch(args);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+        UserService service = retrofit.create(UserService.class);
+        Call<User> callSync = service.getUser("eugenp");
+
+        try {
+            Response<User> response = callSync.execute();
+            User user = response.body();
+            System.out.println(user.getLogin());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
